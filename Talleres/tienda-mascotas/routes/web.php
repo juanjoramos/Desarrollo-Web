092344bf038e85481d38d854139fa8a60d3b03e1
juanjoramos <juan.ramos586@pascualbravo.edu.co>
2025-05-27 18:15:06 -0500

    Poyecto Pia

--- Talleres/tienda-mascotas/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MascotaController;
-
-Route::get('/mascotas', [MascotaController::class, 'index']);
-Route::get('/mascotas/create', [MascotaController::class, 'create']);
-Route::post('/mascotas', [MascotaController::class, 'store']);
